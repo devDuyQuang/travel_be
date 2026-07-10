6380645248df42791ddd53bc393630faf6b9b528
Vo Duy Quang <voquangduy28@gmail.com>
2026-07-10 16:08:35 +0700

    feat: add booking and order management flow

--- database/seeders/DemoServiceOptionsSeeder.php
+++ database/seeders/DemoServiceOptionsSeeder.php
@@ -34,6 +34,11 @@ class DemoServiceOptionsSeeder extends Seeder
                 'unit' => 'đêm',
                 'capacity' => 4,
             ],
+        ], [
+            'location' => 'Đà Nẵng, Việt Nam',
+            'duration' => '25 phút từ trung tâm Đà Nẵng',
+            'review_rating' => 5,
+            'review_count' => 5,
         ]);
 
         $this->syncProductOptions('nha-trang-golf-tour', [
@@ -58,6 +63,11 @@ class DemoServiceOptionsSeeder extends Seeder
                 'price' => 8000000,
                 'unit' => 'khách',
             ],
+        ], [
+            'location' => 'Nha Trang, Việt Nam',
+            'duration' => '3 ngày',
+            'review_rating' => 5,
+            'review_count' => 5,
         ]);
 
         $this->syncProductOptions('thue-xe-du-lich-ok', [
@@ -93,6 +103,13 @@ class DemoServiceOptionsSeeder extends Seeder
                 'unit' => 'chuyến',
                 'capacity' => 9,
             ],
+        ], [
+            'name' => 'Thuê Xe Du Lịch OK',
+            'location' => 'Đà Nẵng, Việt Nam',
+            'duration' => '25 phút từ trung tâm Đà Nẵng',
+            'review_rating' => 5,
+            'review_count' => 5,
+            'badge_text' => 'Giảm 40%',
         ]);
 
         $this->syncProductOptions('pho-co-hoi-an', [
@@ -118,6 +135,12 @@ class DemoServiceOptionsSeeder extends Seeder
                 'unit' => 'gói',
                 'capacity' => 4,
             ],
+        ], [
+            'name' => 'Phố Cổ Hội An',
+            'location' => 'Hội An, Việt Nam',
+            'duration' => '7 ngày',
+            'review_rating' => 5,
+            'review_count' => 5,
         ]);
 
         $this->syncProductOptions('tan-son-nhat-golf-course', [
@@ -142,13 +165,128 @@ class DemoServiceOptionsSeeder extends Seeder
                 'price' => 2800000,
                 'unit' => 'golfer',
             ],
+        ], [
+            'location' => 'TP. Hồ Chí Minh, Việt Nam',
+            'duration' => '18 hố',
+            'review_rating' => 5,
+            'review_count' => 5,
+        ]);
+
+        $this->syncProductOptions('ngu-hanh-son', [
+            [
+                'type' => 'ticket_type',
+                'name' => 'Vé người lớn',
+                'description' => 'Vé tham quan Ngũ Hành Sơn dành cho người lớn',
+                'price' => 250000,
+                'unit' => 'vé',
+            ],
+            [
+                'type' => 'ticket_type',
+                'name' => 'Vé trẻ em',
+                'description' => 'Vé tham quan Ngũ Hành Sơn dành cho trẻ em',
+                'price' => 120000,
+                'unit' => 'vé',
+            ],
+            [
+                'type' => 'ticket_type',
+                'name' => 'Combo gia đình',
+                'description' => 'Gói tham quan gia đình, phù hợp nhóm nhỏ',
+                'price' => 650000,
+                'unit' => 'gói',
+                'capacity' => 4,
+            ],
+        ], [
+            'name' => 'Ngũ Hành Sơn',
+            'location' => 'Đà Nẵng, Việt Nam',
+            'duration' => '2 ngày',
+            'review_rating' => 5,
+            'review_count' => 5,
+        ]);
+
+        $this->syncProductOptions('ba-na-hills', [
+            [
+                'type' => 'ticket_type',
+                'name' => 'Vé người lớn',
+                'description' => 'Vé tham quan Bà Nà Hills dành cho người lớn',
+                'price' => 950000,
+                'unit' => 'vé',
+            ],
+            [
+                'type' => 'ticket_type',
+                'name' => 'Vé trẻ em',
+                'description' => 'Vé tham quan Bà Nà Hills dành cho trẻ em',
+                'price' => 750000,
+                'unit' => 'vé',
+            ],
+            [
+                'type' => 'ticket_type',
+                'name' => 'Combo gia đình',
+                'description' => 'Combo tham quan cho gia đình, tư vấn theo số khách',
+                'price' => 2900000,
+                'unit' => 'gói',
+                'capacity' => 4,
+            ],
+        ], [
+            'name' => 'Bà Nà Hills',
+            'location' => 'Đà Nẵng, Việt Nam',
+            'duration' => '5 ngày',
+            'review_rating' => 5,
+            'review_count' => 5,
+        ]);
+
+        $this->syncProductOptions('thue-limousine', [
+            [
+                'type' => 'vehicle_type',
+                'name' => 'Limousine',
+                'description' => 'Xe limousine cao cấp, phù hợp nhóm nhỏ cần không gian riêng',
+                'price' => 3500000,
+                'unit' => 'chuyến',
+                'capacity' => 9,
+            ],
+        ], [
+            'location' => 'TP. Hồ Chí Minh, Việt Nam',
+            'duration' => '3 ngày',
+            'review_rating' => 5,
+            'review_count' => 5,
+        ]);
+
+        $this->syncProductOptions('thue-carnival', [
+            [
+                'type' => 'vehicle_type',
+                'name' => 'Kia Carnival',
+                'description' => 'Xe gia đình rộng rãi, phù hợp di chuyển nội thành và liên tỉnh',
+                'price' => 1800000,
+                'unit' => 'chuyến',
+                'capacity' => 7,
+            ],
+        ], [
+            'location' => 'Đà Nẵng, Việt Nam',
+            'duration' => '5 ngày',
+            'review_rating' => 5,
+            'review_count' => 5,
+        ]);
+
+        $this->syncProductOptions('thue-fortuner', [
+            [
+                'type' => 'vehicle_type',
+                'name' => 'Toyota Fortuner',
+                'description' => 'Xe SUV phù hợp gia đình hoặc nhóm nhỏ',
+                'price' => 1200000,
+                'unit' => 'chuyến',
+                'capacity' => 7,
+            ],
+        ], [
+            'location' => 'Hà Nội, Việt Nam',
+            'duration' => '7 ngày',
+            'review_rating' => 5,
+            'review_count' => 5,
         ]);
     }
 
     /**
      * @param array<int, array<string, mixed>> $options
      */
-    private function syncProductOptions(string $slug, array $options): void
+    private function syncProductOptions(string $slug, array $options, array $productUpdates = []): void
     {
         $product = Product::query()
             ->where('slug', $slug)
@@ -194,6 +332,7 @@ class DemoServiceOptionsSeeder extends Seeder
             'price_discount' => null,
             'regular_price' => null,
             'sale_price' => null,
+            ...$productUpdates,
         ])->save();
     }
 }
